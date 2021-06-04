4a134731c1b912b32115aa97f5967f2d724fe9d7
Thom Bruce <thom@thombruce.com>
2021-06-04 17:51:32 +0100

    Remove specific socials

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,5 @@
 <template lang='pug'>
 footer
-  .social
-    ul
-      li(v-for='(item, i) in social' :key='i')
-        a(:href='item.to' target='_blank')
-          TntSimpleIcon(:icon='item.icon')
   .copyright &copy; {{ title }} - {{ new Date().getFullYear() }}
 </template>
 
@@ -12,24 +7,7 @@ footer
 export default {
   data () {
     return {
-      title: this.$nuxt.$options.head.title,
-      social: [
-        {
-          icon: 'facebook',
-          title: 'Facebook',
-          to: 'https://facebook.com/thombruce'
-        },
-        {
-          icon: 'twitter',
-          title: 'Twitter',
-          to: 'https://twitter.com/thombruce'
-        },
-        {
-          icon: 'github',
-          title: 'GitHub',
-          to: 'https://github.com/thombruce'
-        },
-      ]
+      title: this.$nuxt.$options.head.title
     }
   }
 }
@@ -37,16 +15,6 @@ export default {
 
 <style lang='postcss' scoped>
 footer {
-  @apply py-3;
-  & .social {
-    @apply text-3xl;
-    & ul {
-      @apply flex justify-center;
-      & li a {
-        @apply block px-4;
-      }
-    }    
-  }
   & .copyright {
     @apply text-center text-sm text-gray-400 dark:text-gray-500;
   }
