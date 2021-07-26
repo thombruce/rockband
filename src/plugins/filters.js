c94ce8ca065e5a1d92e8a2acfb3b8d0136cbc46b
Thom Bruce <thom@thombruce.com>
2021-07-26 17:28:34 +0100

    Remove duplicate filter

--- src/plugins/filters.js
+++ src/plugins/filters.js
@@ -6,6 +6,4 @@ import { DateTime } from 'luxon'
 
 Vue.filter('titleize', val => startCase(val))
 
-Vue.filter('toLocaleString', val => new Date(val).toLocaleDateString())
-
 Vue.filter('toLocaleString', val => DateTime.fromISO(val).toLocaleString(DateTime.DATETIME_MED))
