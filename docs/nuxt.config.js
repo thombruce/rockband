f5dde04f4a3d6273da1141e6691f736f26aa808b
Thom Bruce <thom@thombruce.com>
2021-08-04 21:30:18 +0100

    Enable themeToggle to handle custom themes and subsets of themes

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -8,6 +8,9 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'TNT',
+    htmlAttrs: {
+      'data-theme': 'tnt'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- src/components/UI/ThemeToggle.vue
+++ src/components/UI/ThemeToggle.vue
@@ -1,40 +1,55 @@
 <template lang="pug">
-.dropdown.dropdown-end
+.dropdown.dropdown-end(v-if='themes')
   .m-1.btn.btn-ghost(tabindex='0') Theme
   ul.shadow.menu.dropdown-content.bg-base-200.text-base-content.rounded-box.w-52
     li(v-for="(theme, index) in themes")
-      a(tabindex="0" :data-set-theme="theme.id" data-act-class="active") {{ theme.name }}
+      a(tabindex="0" :data-set-theme="theme" data-act-class="active") {{ theme | titleize }}
 </template>
 
 <script>
 import { themeChange } from 'theme-change'
 
+// Conditionally require tailwind.config if it exists.
+let tailwindConfig
+try { tailwindConfig = require('~/tailwind.config') }
+catch(e) {}
+
+const defaultThemes = [
+  'light',
+  'dark',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+  'halloween',
+  'garden',
+  'forest',
+  'aqua',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'wireframe',
+  'black',
+  'luxury',
+  'dracula',
+]
+
+const themes = (tailwindConfig && tailwindConfig.daisyui && tailwindConfig.daisyui.themes)
+  ?
+    (Array.isArray(tailwindConfig.daisyui.themes) ? tailwindConfig.daisyui.themes : defaultThemes)
+  :
+    defaultThemes // TODO: Differentiate between false and undefined. Undefined should show defaultThemes, false should not.
+
+const themeKeys = themes.map(theme => typeof theme === 'object' ? Object.keys(theme).pop() : theme)
+
 export default {
   data () {
     return {
-      themes: [
-        { id: "light", name: "🌝  light" },
-        { id: "dark", name: "🌚  dark" },
-        { id: "cupcake", name: "🧁  cupcake" },
-        { id: "bumblebee", name: "🐝  bumblebee" },
-        { id: "emerald", name: "✳️  Emerald" },
-        { id: "corporate", name: "🏢  Corporate" },
-        { id: "synthwave", name: "🌃  synthwave" },
-        { id: "retro", name: "👴  retro" },
-        { id: "cyberpunk", name: "🤖  cyberpunk" },
-        { id: "valentine", name: "🌸  valentine" },
-        { id: "halloween", name: "🎃  halloween" },
-        { id: "garden", name: "🌷  garden" },
-        { id: "forest", name: "🌲  forest" },
-        { id: "aqua", name: "🐟  aqua" },
-        { id: "lofi", name: "👓  lofi" },
-        { id: "pastel", name: "🖍  pastel" },
-        { id: "fantasy", name: "🧚‍♀️  fantasy" },
-        { id: "wireframe", name: "📝  Wireframe" },
-        { id: "black", name: "🏴  black" },
-        { id: "luxury", name: "💎  luxury" },
-        { id: "dracula", name: "🧛‍♂️  dracula" },
-      ],
+      themes: themeKeys,
     }
   },
   mounted () {
