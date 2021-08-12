986c4df5bcccae0b97ee13251966c68144732569
Thom Bruce <thom@thombruce.com>
2021-08-12 17:43:45 +0100

    Automatic sidebar menu for docs theme

--- src/components/layouts/docs/sidebar/Menu.vue
+++ src/components/layouts/docs/sidebar/Menu.vue
@@ -1,7 +1,40 @@
 <template lang='pug'>
-div
+div.h-full
+  ul.menu.flex.flex-col.p-4(v-for='(collection, dir) in collections')
+    li.menu-title.mt-2
+      span
+        NuxtLink(:to='dir') {{ dir | titleize }}
+    li(v-for='page in collection' :key='page.slug')
+      NuxtLink(:to='page') {{ page.title }}
+
+    li.menu-title.mt-2
+      span Other Pages
+    li(v-for='page in pages' :key='page.slug')
+      NuxtLink(:to='page') {{ page.title }}
 </template>
 
 <script>
-export default {}
+import { groupBy } from 'lodash'
+
+export default {
+  data () {
+    return {
+      collections: null,
+      pages: null
+    }
+  },
+  async fetch() {
+    const nestedPages = await this.$content({ deep: true })
+      .where({ draft: { $ne: true }, dir: { $ne: '/' } })
+      .fetch()
+      .catch(() => {})
+
+    this.collections = groupBy(nestedPages, 'dir')
+
+    this.pages = await this.$content()
+      .where({ draft: { $ne: true } })
+      .fetch()
+      .catch(() => {})
+  },
+}
 </script>
