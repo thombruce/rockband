fab890be18909536b404745ef57b70a2d89b16de
Thom Bruce <thom@thombruce.com>
2021-08-31 19:59:07 +0100

    Add ThemeChange component and FontAwesome

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import VueMeta from 'vue-meta'
 
 import './assets/index.css'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 export default {
   install (Vue, options) {
     Vue.use(VueMeta)
+
+    Vue.component('fa', FontAwesomeIcon)
   }
 }
