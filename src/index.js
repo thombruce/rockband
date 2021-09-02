cceb24601d278111377401eb96ae24100ce85c23
Thom Bruce <thom@thombruce.com>
2021-09-02 23:41:54 +0100

    Nuxt specific CSS

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ export default async function () {
 
   nuxt.options.tailwindcss = {
     ...{
+      cssPath: fs.existsSync("assets/css/tnt.css") ? "~/assets/css/tnt.css" : join(__dirname, "assets/tnt.css"),
       mode: 'jit',
       config: {
         purge: {
