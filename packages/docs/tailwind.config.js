1311b28523781cbf0244e72db6d9ed3bff118741
Thom Bruce <thom@thombruce.com>
2021-09-27 23:59:40 +0100

    Modify purge path

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       'layouts/**/*.vue',
       'plugins/**/*.{js,ts}',
       'content/**/*.md',
-      '../packages/**/*.vue',
+      '../**/*.vue',
     ]
   },
   daisyui: {
