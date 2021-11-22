a621a60d137a8e8bd62c135d903f40b4bbbdefd3
Thom Bruce <thom@thombruce.com>
2021-11-22 10:52:28 +0000

    Change router base path

--- packages/docs/nuxt.config.js
+++ packages/docs/nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/tnt/'
+    base: '/ui/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
