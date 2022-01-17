0653ed8677e83f3b9bdf827799977d854768e82a
Thom Bruce <thom@thombruce.com>
2022-01-17 03:22:18 +0000

    Cleaner title styles

--- packages/nuxt/src/components/layouts/classic/header/Title.vue
+++ packages/nuxt/src/components/layouts/classic/header/Title.vue
@@ -15,6 +15,6 @@ export default {
 
 <style lang='postcss' scoped>
 span {
-  @apply block w-min m-auto text-4xl sm:text-6xl md:text-7xl;
+  @apply block w-min m-auto whitespace-nowrap text-4xl sm:text-5xl;
 }
 </style>
