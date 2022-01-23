904e0a43f23773dce85bd1afd900560434da320f
thombruce <thombruce@users.noreply.github.com>
2022-01-23 14:40:48 +0000

    deploy: b6795f5e9d028e9675fea06db027ad2d8dd7e574

--- _nuxt/static/1642948347/features/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fui\u002F_nuxt\u002Fstatic\u002F1642948347",layout:"default",error:a,serverRendered:b,routePath:"\u002Ffeatures",config:{_app:{basePath:"\u002Fui\u002F",assetsPath:"\u002Fui\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a5ec9c2e"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
