8299cb3666f65facb2447728a1094b2887d23521
Thom Bruce <thom@thombruce.com>
2022-01-23 14:41:20 +0000

    Update site root

--- packages/docs/nuxt.config.js
+++ packages/docs/nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/ui/'
+    base: '/rockband/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
