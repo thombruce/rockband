77d4efbdeb65dad00bbf7c7d40f52b3a53254ebe
thombruce <thombruce@users.noreply.github.com>
2022-01-24 17:42:20 +0000

    deploy: de4f1acb23118b738ae8fd7b23d77c04d853197f

---
+++ _nuxt/8d124f9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{4581:function(t,e,n){"use strict";n.r(e);var r={props:["article"]},c=n(5),component=Object(c.a)(r,(function(){var t=this,e=t.$createElement;return(t._self._c||e)("NuxtContent",{staticClass:"prose dark:prose-invert",attrs:{document:t.article}})}),[],!1,null,null,null);e.default=component.exports}}]);
--- _nuxt/static/1643038244/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fsetlist","\u002Fcredits","\u002Ffeatures","\u002Fcomponents","\u002Fgetting-started","\u002Fgetting-started\u002Fconfiguration","\u002Fcomponents\u002Fbreadcrumbs","\u002Fgetting-started\u002Finstallation"]})
